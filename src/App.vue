584f9bcd43831b9b811bc657c2163202d226b848
Muhi Masri <Muhi.Masri@mdacorporation.com>
2021-10-06 21:23:08 -0400

    Add and remove rows

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,32 @@
 <template>
   <div id="app">
-    <article>
-      <h3>Employee Information</h3>
-      <EditTableRow v-model="items1" :fields="fields1"></EditTableRow>
-    </article>
-    <article>
-      <h3>Employee Contract</h3>
-      <EditTableRow v-model="items2" :fields="fields2"></EditTableRow>
-    </article>
+      <EditableTable v-model="items" :fields="fields"></EditableTable>
   </div>
 </template>
 
 <script>
-import EditTableRow from './components/EditTableRow.vue';
+import EditableTable from './components/EditableTable.vue';
 
 export default {
   name: "App",
   components: {
-    EditTableRow
+    EditableTable
   },
   data() {
     return {
-      fields1: [
+      fields: [
+        { key: "selectRow", label: "", type: "checkbox" },
         { key: "name", label: "Name", type: "text" },
         { key: "department", label: "Department", type: "select", options: ['Development', 'Marketing', 'HR', 'Accounting'] },
         { key: "age", label: "Age", type: "number" },
         { key: "dateOfBirth", label: "Date Of Birth", type: "date" },
         { key: "edit", label: "", type: "edit" }
       ],
-       items1: [
+       items: [
           { age: 40, name: 'Dickerson', department: 'Development', dateOfBirth: '1984-05-20' },
           { age: 21, name: 'Larsen', department: 'Marketing', dateOfBirth: '1984-05-20' },
           { age: 89, name: 'Geneva', department: 'HR', dateOfBirth: '1984-05-20' },
           { age: 38, name: 'Jami', department: 'Accounting', dateOfBirth: '1984-05-20' }
-        ],
-        fields2: [
-          { key: "id", label: "ID", type: "number" },
-          { key: "name", label: "Name", type: "text" },
-          { key: "contractEndDate", label: "Contract End Date", type: "date" },
-          { key: "edit", label: "", type: "edit" }
-      ],
-       items2: [
-          { id: 123, name: 'Dickerson', contractEndDate: '2022-05-20' },
-          { id: 345, name: 'Larsen', contractEndDate: '2021-05-20' },
-          { id: 456, name: 'Geneva', contractEndDate: '2023-05-20' },
-          { id: 678, name: 'Jami', contractEndDate: '2024-05-20' }
         ]
     };
   }
@@ -53,11 +35,6 @@ export default {
 
 <style>
 #app {
-  text-align: center;
-  margin: 60px;
-}
-pre {
-  text-align: left;
-  color: #d63384;
+  margin: 20px;
 }
 </style>
---
+++ src/components/EditableTable.vue
@@ -0,0 +1,82 @@
+<template>
+    <article>
+       <b-modal id="modal-1" title="Confirm" v-model="openDialog" ok-title="Remove" @ok="removeRowsHandler">
+        <p class="my-4">Are you sure you want to remove the selected rows?</p>
+      </b-modal>
+        <div class="action-container">
+          <b-button class="add-button" variant="success" @click="addRowHandler">Add Row</b-button>
+          <b-button variant="danger" @click="openDialog = true">Delete Rows</b-button>
+        </div>
+        <b-table class="b-table" :items="tableItems" :fields="fields" fixed>
+            <template v-for="(field, index) in fields" #[`cell(${field.key})`]="data">
+                <b-form-datepicker v-if="field.type === 'date' && tableItems[data.index].isEdit" :key="index" :type="field.type" :value="tableItems[data.index][field.key]" @input="(value) => inputHandler(value, data.index, field.key)"></b-form-datepicker>
+                <b-form-select v-else-if="field.type === 'select' && tableItems[data.index].isEdit" :key="index" :value="tableItems[data.index][field.key]" @input="(value) => inputHandler(value, data.index, field.key)" :options="field.options"></b-form-select>
+                <b-checkbox  v-else-if="field.type === 'checkbox' && field.key === 'selectRow'" :checked="tableItems[data.index].isSelected" :key="index" @change="selectRowHandler(data)"></b-checkbox>
+                <div :key="index" v-else-if="field.type === 'edit'">
+                    <b-button @click="editRowHandler(data)">
+                        <span v-if="!tableItems[data.index].isEdit">Edit</span>
+                        <span v-else>Done</span>
+                    </b-button>
+                    <b-button class="delete-button" variant="danger" @click="removeRowHandler(data.index)">Remove</b-button>
+                </div>
+                <b-form-input v-else-if="field.type && tableItems[data.index].isEdit" :key="index" :type="field.type" :value="tableItems[data.index][field.key]" @input="(value) => inputHandler(value, data.index, field.key)"></b-form-input>
+                <span :key="index" v-else>{{data.value}}</span>
+            </template>
+        </b-table>
+    </article>
+</template>
+
+<script>
+export default {
+  name: "EditableTable",
+  components: {},
+  props: {
+    value: Array,
+    fields: Array
+  },
+  data() {
+    return {
+      tableItems: this.value.map(item => ({...item, isEdit: false, isSelected: false})),
+      openDialog: false
+    }
+  },
+  methods: {
+      editRowHandler(data) {
+        this.tableItems[data.index].isEdit = !this.tableItems[data.index].isEdit;
+      },
+      inputHandler(value, index, key) {
+        this.tableItems[index][key] = value;
+        this.$emit('input', this.tableItems);
+      },
+      addRowHandler() {
+        const newRow = this.fields.reduce((a, c) => ({...a, [c.key]: null}) ,{})
+        newRow.isEdit = true;
+        this.tableItems.unshift(newRow);
+        this.$emit('input', this.tableItems);
+      },
+      removeRowHandler(index) {
+        this.tableItems = this.tableItems.filter((item, i) => i !== index);
+        this.$emit('input', this.tableItems);
+      },
+      removeRowsHandler() {
+        this.tableItems = this.tableItems.filter(item => !item.isSelected);
+        this.$emit('input', this.tableItems);
+      },
+      selectRowHandler(data) {
+        this.tableItems[data.index].isSelected = !this.tableItems[data.index].isSelected;
+      }
+    }
+};
+</script>
+
+<style>
+    .action-container {
+        margin-bottom: 10px;
+    }
+    .action-container button {
+        margin-right: 5px;
+    }
+    .delete-button {
+        margin-left: 5px;
+    }
+</style>
